41a3576bd2ccb62d0407f8d895e00307d97c56f4
Kenny Victoria <kvict3@gmail.com>
2016-07-02 20:48:30 -0400

    mysql added

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('bacon cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('breakfast burger');
+INSERT INTO burgers (burger_name) VALUES ('bbq burger');
+INSERT INTO burgers (burger_name) VALUES ('monster burger');
+INSERT INTO burgers (burger_name) VALUES ('mini burger');
